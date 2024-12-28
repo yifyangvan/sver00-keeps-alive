43ad5a428c26be57fbb4a396068e7928adfedb18
ryty1 <47910814+ryty1@users.noreply.github.com>
2024-12-28 13:53:11 +0800

    Update serv00_server.sh
    1、修复登陆失败推送消息服务器显示为空白的错误。
    2、调整逻辑，减少多余代码，优化SSH连接问题，提高执行效率。
    3、简化脚本打印输出内容。

--- serv00_server.sh
+++ serv00_server.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-export ACCOUNTS="$ACCOUNTS"
 WXPUSHER_TOKEN="$WXPUSHER_TOKEN"
 PUSHPLUS_TOKEN="$PUSHPLUS_TOKEN"
-WXPUSHER_USER_ID="$WXPUSHER_USER_ID"
 TG_BOT_TOKEN="$TG_BOT_TOKEN"
 TG_CHAT_ID="$TG_CHAT_ID"
 WXPUSHER_URL="https://wxpusher.zjiecode.com/api/send/message"
 PUSHPLUS_URL="http://www.pushplus.plus/send"
 TELEGRAM_URL="https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage"
-RESULT_SUMMARY="青龙自动进程内容：\n———————————————————————\n 哪吒V1面板 ‖ 探针 ‖ singbox ‖ sun-panel ‖ webssh ‖ alist \n———————————————————————\n"
+RESULT_SUMMARY="青龙自动进程内容：\n———————————————————————\n"
 send_wxpusher_message() {
   local title="$1"
   local content="$2"
@@ -59,88 +57,83 @@ mask_server() {
   local server="$1"
   echo "$server" | cut -d '.' -f 1
 }
-LOGIN_TIMEOUT=20
-IFS='|' read -r -a account_info <<< "$ACCOUNTS"
-index=1
-echo "正在进行 {账号+密码+服务器} 拆解重组....  "
-echo ""
-for (( i=0; i<${#account_info[@]}; i++ )); do
-  USERNAME=$(echo "${account_info[$i]}" | cut -d ',' -f 1)
-  PASSWORD=$(echo "${account_info[$i]}" | cut -d ',' -f 2)
-  SERVER_LIST=$(echo "${account_info[$i]}" | cut -d ',' -f 3)
-  OPERATION_RESULT="❌"
-  IFS=':' read -r -a servers <<< "$SERVER_LIST"
-  for SERVER in "${servers[@]}"; do
-    echo "尝试使用 [$USERNAME] 账号 登录 [$SERVER] 服务器  "
-    sshpass -p "$PASSWORD" timeout $LOGIN_TIMEOUT ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=QUIET -T "$USERNAME@$SERVER" exit
-    SSH_EXIT_CODE=$?
-    if [[ $SSH_EXIT_CODE -eq 0 ]]; then
-      echo "恭喜这个逼，登录成功，命令执行开始：  "
-      MASKED_USERNAME=$(mask_username "$USERNAME")
-      MASKED_SERVER=$(mask_server "$SERVER")
-      OPERATION_RESULT="✅"  # 登录成功即记录为 ✅
-      RESULT_SUMMARY+="✅      $index. $MASKED_USERNAME       【 $MASKED_SERVER 】登录成功\n"
-      sshpass -p "$PASSWORD" timeout $LOGIN_TIMEOUT ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=QUIET -T "$USERNAME@$SERVER" <<EOF
-    echo " "
-    echo "       ✅ 拉取成功          ❌ 没有安装"
+mask_server_out() {
+  local servers="$1"
+  first_server=$(echo "$servers" | cut -d ':' -f 1 | cut -d ',' -f 1)  
+  echo "$first_server" | cut -d '.' -f 1
+}
+manage_services() {
+  local USERNAME="$1"
+  local PASSWORD="$2"
+  local SERVER="$3"
+  $CMD <<EOF
     echo "——————————————————————————————————————————————————"
 if [ -d "/home/$USERNAME/nezha_app/dashboard" ]; then
     pkill -f "nezha-dashboard" >/dev/null 2>&1 || true
     cd /home/$USERNAME/nezha_app/dashboard && nohup ./nezha-dashboard >/dev/null 2>&1 & 
     echo "✅ -------- 哪吒V1面板"
-else
-    echo "❌ -------- 哪吒V1面板"
 fi
 if [ -d "/home/$USERNAME/nezha_app/agent" ]; then
     pkill -f "nezha-agent" >/dev/null 2>&1 || true
     cd /home/$USERNAME/nezha_app/agent && nohup sh nezha-agent.sh >/dev/null 2>&1 &
     echo "✅ -------- V1探针"
-else
-    echo "❌ -------- V1探针"
 fi
 if [ -d "/home/$USERNAME/serv00-play/nezha" ] && [ -f "/home/$USERNAME/serv00-play/nezha/nezha.json" ]; then
     pkill -f "nezha-agent" >/dev/null 2>&1 || true
     cd /home/$USERNAME/serv00-play/nezha
-    nohup ./nezha-agent --report-delay 4 --disable-auto-update --disable-force-update \
+    nohup ./nezha-agent --report-delay 4 --disable-auto-update --disable-force-update --delay=2 \
         $( [[ "$(jq -r '.tls' nezha.json 2>/dev/null)" == "y" ]] && echo "--tls" ) \
         -s "$(jq -r '.nezha_domain' nezha.json 2>/dev/null):$(jq -r '.nezha_port' nezha.json 2>/dev/null)" \
         -p "$(jq -r '.nezha_pwd' nezha.json 2>/dev/null)" >/dev/null 2>&1 &
     echo "✅ -------- V0探针"
-else
-    echo "❌ -------- V0探针"
 fi
 if [ -d "/home/$USERNAME/serv00-play/singbox" ]; then
-    cd /home/$USERNAME/serv00-play/singbox && nohup ./start.sh >/dev/null 2>&1 &
+    cd /home/$USERNAME/serv00-play/singbox && nohup ./start.sh --delay=2 >/dev/null 2>&1 &
     echo "✅ -------- singbox"
-else
-    echo "❌ -------- singbox"
 fi
 if [ -d "/home/$USERNAME/serv00-play/sunpanel" ]; then
     pkill -f "sun-panel" >/dev/null 2>&1 || true
-    cd /home/$USERNAME/serv00-play/sunpanel && nohup ./sun-panel >/dev/null 2>&1 &
+    cd /home/$USERNAME/serv00-play/sunpanel && nohup ./sun-panel --delay=2 >/dev/null 2>&1 &
     echo "✅ -------- sun-panel"
-else
-    echo "❌ -------- sun-panel"
 fi
 if [ -d "/home/$USERNAME/serv00-play/webssh" ] && [ -f "/home/$USERNAME/serv00-play/webssh/config.json" ]; then
     cd /home/$USERNAME/serv00-play/webssh
-    nohup ./wssh --port=$(jq -r ".port" config.json 2>/dev/null) --fbidhttp=False --xheaders=False --encoding="utf-8" --delay=10 >/dev/null 2>&1 &
+    nohup ./wssh --port=$(jq -r ".port" config.json 2>/dev/null) --fbidhttp=False --xheaders=False --encoding="utf-8" --delay=5 >/dev/null 2>&1 &
     echo "✅ -------- webssh"
-else
-    echo "❌ -------- webssh"
 fi
 if [ -d "/home/piaoc/serv00-play/alist" ]; then
     pkill -f "alist server" >/dev/null 2>&1 || true
-    cd /home/piaoc/serv00-play/alist && nohup ./alist server >/dev/null 2>&1 &
+    cd /home/piaoc/serv00-play/alist && nohup ./alist server --delay=5 >/dev/null 2>&1 &
     echo "✅ -------- alist"
-else
-    echo "❌ -------- alist"
 fi
     echo "——————————————————————————————————————————————————"
-
-        ps -A >/dev/null 2>&1
-        exit
 EOF
+}
+LOGIN_TIMEOUT=10
+index=1
+echo "正在进行 {账号+密码+服务器} 拆解重组....  "
+echo ""
+IFS='|' read -r -a account_info <<< "$ACCOUNTS"
+for (( i=0; i<${#account_info[@]}; i++ )); do
+  IFS=',' read -r USERNAME PASSWORD SERVER_LIST <<< "${account_info[$i]}"
+  OPERATION_RESULT="❌"
+  IFS=':' read -r -a servers <<< "$SERVER_LIST"
+  for SERVER in "${servers[@]}"; do
+    echo "尝试使用 [$USERNAME] 账号 登录 [$SERVER] 服务器..."
+    CMD="sshpass -p "$PASSWORD" timeout $LOGIN_TIMEOUT ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=QUIET -T "$USERNAME@$SERVER""
+    $CMD exit
+    SSH_EXIT_CODE=$?
+    if [[ $SSH_EXIT_CODE -eq 0 ]]; then
+      echo "恭喜这个逼，登录成功！开始执行命令："
+      MASKED_USERNAME=$(mask_username "$USERNAME")
+      MASKED_SERVER=$(mask_server "$SERVER")
+      OPERATION_RESULT="✅"
+      RESULT_SUMMARY+="✅      $index. $MASKED_USERNAME       【 $MASKED_SERVER 】   登录成功\n"
+      manage_services "$USERNAME" "$PASSWORD" "$SERVER"
+      process_list=$($CMD "ps -A")
+      echo "生成进程结果明细汇总到推送消息中.....  "
+      echo "  "
+      PROCESS_DETAILS=""
       declare -A processes=(
         ["nezha-dashboard"]="哪吒面板"
         ["nezha-agent"]="探针"
@@ -149,10 +142,6 @@ EOF
         ["wssh"]="webssh"
         ["alist server"]="alist"
       )
-      process_list=$(sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=QUIET -T "$USERNAME@$SERVER" "ps -A")
-      echo "正在进行进程对比......  "
-      PROCESS_DETAILS=""
-
       for process in "${!processes[@]}"; do
         if echo "$process_list" | grep -q "$process"; then
           PROCESS_DETAILS+="    ${processes[$process]} |"
@@ -163,18 +152,16 @@ EOF
       else
         OPERATION_RESULT="❌"
       fi
-      echo "对比结束，已启动进程记录在结果中。  "
-      echo "  "
       RESULT_SUMMARY+=" ------ $PROCESS_DETAILS 已启动\n"
-      
-      break 
+      break
     else
       echo "登录失败，尝试下一个服务器..."
     fi
   done
   if [[ "$OPERATION_RESULT" == "❌" ]]; then
     MASKED_USERNAME=$(mask_username "$USERNAME")
-    RESULT_SUMMARY+="❌      $index. $MASKED_USERNAME       【 $MASKED_SERVER 】 登录失败\n"
+    MASKED_SERVER_OUT=$(mask_server_out "$SERVER_LIST")
+    RESULT_SUMMARY+="❌      $index. $MASKED_USERNAME       【 $MASKED_SERVER_OUT 】   登录失败\n"
   fi
   ((index++))
 done
