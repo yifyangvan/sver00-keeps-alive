7406b66939f6acb84c8502d4074a38726c2fa956
人皇帝辛 <120008435@qq.com>
2025-01-23 15:18:20 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ D2="$R1/$D1"
 N1="$D2/public_nodejs"
 F1="$N1/app.js"
 L1="https://raw.githubusercontent.com/ryty1/sver00-save-me/refs/heads/main/app.js"
-
 echo " ———————————————————————————————————————————————————————————— "
+echo ""
 devil www del "$D1" > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
     echo " [OK] 默认域名 已删除 "
@@ -36,8 +36,7 @@ if [[ ! -d "$N1" ]]; then
 fi
 
 if npm install dotenv basic-auth express > /dev/null 2>&1; then
-    echo " [OK] 环境依赖 安装成功
-    "
+    echo " [OK] 环境依赖 安装成功"
 else
     echo "依赖安装失败，请检查 Node.js 环境。"
     exit 1
@@ -45,11 +44,12 @@ fi
 
 if curl -s -o "$F1" "$L1"; then
     echo " [OK] 配置文件 下载成功 "
+    echo ""
 else
     echo "配置文件下载失败，请检查 URL。"
     exit 1
 fi
-
+echo " ———————————————————————————————————————————————————————————— "
 echo " 【 恭 喜 】： 网 页 保 活 一 键 部 署 已 完 成  "
 echo " ———————————————————————————————————————————————————————————— "
 echo " |**保活网页 https://$D1/info "
